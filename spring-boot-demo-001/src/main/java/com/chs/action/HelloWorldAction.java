a87e87e075467baceaf3d30b2daae43f6e282881
upsheng <fjndchs@163.com>
2019-06-22 12:04:14 +0800

    001

---
+++ spring-boot-demo-001/src/main/java/com/chs/action/HelloWorldAction.java
@@ -0,0 +1,36 @@
+package com.chs.action;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <pre>
+ *
+ * </pre>
+ *
+ * @author 陈洪生<br>
+ * <b>mail</b> chenhongsheng@tansun.com.cn<br>
+ * <b>date</b> 2019/6/22<br>
+ * @version 1.0.1
+ * <pre>
+ * 版本            修改人            修改日期            修改内容描述
+ * --------------------------------------------------------------------
+ * 1.0.1 	       陈洪生	        2019/6/22            创建
+ * --------------------------------------------------------------------
+ * </pre>
+ */
+
+@RestController
+
+public class HelloWorldAction {
+
+    @RequestMapping("/helloWorld")
+    public Map<String,Object> helloWorld(){
+        Map<String,Object>  map = new HashMap<>();
+        map.put("msg","helloWorld");
+        return  map;
+    }
+}
