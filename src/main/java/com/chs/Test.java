e99dc4afcf4033951de1128033e08dde8a591a3b
upsheng <fjndchs@163.com>
2019-06-22 11:39:54 +0800

    1

---
+++ src/main/java/com/chs/Test.java
@@ -0,0 +1,23 @@
+package com.chs;
+
+/**
+ * <pre>
+ *
+ * </pre>
+ *
+ * @author 陈洪生<br>
+ * <b>mail</b> chenhongsheng@tansun.com.cn<br>
+ * <b>date</b> 2019/6/22<br>
+ * @version 1.0.1
+ * <pre>
+ * 版本            修改人            修改日期            修改内容描述
+ * --------------------------------------------------------------------
+ * 1.0.1 	       陈洪生	        2019/6/22            创建
+ * --------------------------------------------------------------------
+ * </pre>
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("111");
+    }
+}
