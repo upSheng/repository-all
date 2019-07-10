6cb5145cb965c2cfc2fa05c534783c8bc181fdb1
upsheng <fjndchs@163.com>
2019-07-10 17:17:03 +0800

    security

---
+++ spring-boot-demo-security/src/main/java/com/chs/security3/config/SelfAuthenticationProvider.java
@@ -0,0 +1,41 @@
+package com.chs.security3.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class SelfAuthenticationProvider implements AuthenticationProvider {
+    @Autowired
+    SelfUserDetailsService userDetailsService;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String userName = (String) authentication.getPrincipal(); // 这个获取表单输入中返回的用户名;
+        String password = (String) authentication.getCredentials(); // 这个是表单中输入的密码；
+
+//        Md5PasswordEncoder md5PasswordEncoder = new Md5PasswordEncoder();
+//        String encodePwd = md5PasswordEncoder.encodePassword(password, userName);
+
+
+        String encodePwd = password;
+        UserDetails userInfo = userDetailsService.loadUserByUsername(userName);
+
+        if (!userInfo.getPassword().equals(encodePwd)) {
+            throw new BadCredentialsException("用户名密码不正确，请重新登陆！");
+        }
+
+        return new UsernamePasswordAuthenticationToken(userName, password, userInfo.getAuthorities());
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return true;
+    }
+}
---
+++ spring-boot-demo-security/src/main/java/com/chs/security3/controller/testController.java
@@ -0,0 +1,13 @@
+package com.chs.security3.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testController {
+
+    @GetMapping("index")
+    public String index(){
+        return "Hello";
+    }
+}
