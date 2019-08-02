e23a31cf0987cf938cf5cb1a49c4848b6a88fd67
upsheng <fjndchs@163.com>
2019-08-02 11:03:54 +0800

    jwt

--- spring-boot-demo-security/src/main/java/com/chs/security3/controller/testController.java
+++ spring-boot-demo-security/src/main/java/com/chs/security3/controller/testController.java
@@ -1,5 +1,7 @@
 package com.chs.security3.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +12,12 @@ public class testController {
 
     @GetMapping("/index")
     public String index(){
-        return "index";
+
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext()
+                .getAuthentication()
+                .getPrincipal();
+        String name = userDetails.getUsername();
+        return name;
     }
 
     @GetMapping("/inner")
