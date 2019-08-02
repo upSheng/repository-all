1fa71e5f338c0ace5442c4b7216e89b05eef7a05
upsheng <fjndchs@163.com>
2019-08-02 11:45:26 +0800

    web

---
+++ spring-boot-demo-web/src/main/java/com/chs/config/ErrorPageConfig.java
@@ -0,0 +1,40 @@
+package com.chs.config;
+
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+
+/**
+ * <pre>
+ *
+ * </pre>
+ *
+ * @author 陈洪生<br>
+ * <b>mail</b> chenhongsheng@tansun.com.cn<br>
+ * <b>date</b> 2019/8/2<br>
+ * @version 1.0.1
+ * <pre>
+ * 版本            修改人            修改日期            修改内容描述
+ * --------------------------------------------------------------------
+ * 1.0.1 	       陈洪生	        2019/8/2            创建
+ * --------------------------------------------------------------------
+ * </pre>
+ */
+@Configuration
+public class ErrorPageConfig {
+    @Bean
+    public WebServerFactoryCustomizer<ConfigurableWebServerFactory> webServerFactoryCustomizer() {
+        return new WebServerFactoryCustomizer<ConfigurableWebServerFactory>() {
+            @Override
+            public void customize(ConfigurableWebServerFactory factory) {
+                ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND,"/index.html");
+                factory.addErrorPages(errorPage404);
+            }
+        };
+    }
+
+
+}
