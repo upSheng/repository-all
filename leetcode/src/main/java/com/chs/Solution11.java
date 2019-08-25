88d709d669f86369eb1d4bbe35dae95acf7bc49b
chs <fjndchs@163.com>
2019-08-25 11:26:32 +0800

    11

--- leetcode/src/main/java/com/chs/Solution11.java
+++ leetcode/src/main/java/com/chs/Solution11.java
@@ -3,26 +3,20 @@ package com.chs;
 public class Solution11 {
 
     public static void main(String[] args) {
-        int[] arry = {3,4,2,6,10,8};
-
+        int[] arry = {3, 4, 2, 6, 10, 8};
         System.out.println(solution(arry));
-
-
     }
 
-    public static int solution(int[] height){
-
-
+    public static int solution(int[] height) {
         int maxArea = 0;
         int l = 0;
         int r = height.length - 1;
 
-        while (l<r){
-            maxArea = Math.max(maxArea,Math.min(height[l],height[r])*(r-l));
-
-            if(height[l]<height[r]){
+        while (l < r) {
+            maxArea = Math.max(maxArea, Math.min(height[l], height[r]) * (r - l));
+            if (height[l] < height[r]) {
                 l++;
-            }else {
+            } else {
                 r--;
             }
         }
