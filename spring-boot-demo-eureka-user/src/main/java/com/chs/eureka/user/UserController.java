a37ee6750b97079bc365836e57612b2f0e6aefc6
chs <fjndchs@163.com>
2019-09-15 00:12:27 +0800

    ribbon

--- spring-boot-demo-eureka-user/src/main/java/com/chs/eureka/user/UserController.java
+++ spring-boot-demo-eureka-user/src/main/java/com/chs/eureka/user/UserController.java
@@ -1,5 +1,6 @@
 package com.chs.eureka.user;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,14 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
 
+    @Value("${server.port}")
+    private int port;
+
     @RequestMapping("/user/{id}")
     public String getUser(@PathVariable("id") int id){
 
 
         if(id==1){
-            return "张三";
+            return "张三" + this.port;
         }else {
-            return "李四";
+            return "李四" + this.port;
         }
     }
 }
