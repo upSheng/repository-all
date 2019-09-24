085235b50aecd5a491a5050c008af32b94560271
chs <fjndchs@163.com>
2019-09-25 01:00:52 +0800

    schedule

--- spring-boot-demo-web/src/main/java/com/chs/config/SecurityConfig.java
+++ spring-boot-demo-web/src/main/java/com/chs/config/SecurityConfig.java
@@ -1,12 +1,20 @@
 package com.chs.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
-@EnableWebSecurity
+//@EnableWebSecurity
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    SelfUserDetailsService selfUserDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -28,5 +36,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         //默认都会产生一个hiden标签 里面有安全相关的验证 防止请求伪造 这边我们暂时不需要 可禁用掉
         http .csrf().disable();
 
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        // 加入自定义的安全认证
+        auth.userDetailsService(selfUserDetailsService).passwordEncoder(new BCryptPasswordEncoder());
     }
 }
