0e2a9b01e533179f3783617067170a9294843693
chs <fjndchs@163.com>
2019-09-29 22:01:17 +0800

    Hystri

--- spring-boot-demo-eureka-order/src/main/java/com/chs/eureka/order/Application.java
+++ spring-boot-demo-eureka-order/src/main/java/com/chs/eureka/order/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -26,6 +27,7 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrix
 public class Application {
 
     public static void main(String[] args) {
