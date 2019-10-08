ca55b6f68857e3b91499245e45d6a27c60712a87
chs <fjndchs@163.com>
2019-10-08 21:47:58 +0800

    leet

---
+++ leetcode/src/main/java/com/chs/ListNode.java
@@ -0,0 +1,13 @@
+package com.chs;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+
+}
+
+
---
+++ leetcode/src/main/java/com/chs/Solution2.java
@@ -0,0 +1,22 @@
+package com.chs;
+
+public class Solution2 {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode result = new ListNode(1);
+
+
+        ListNode node = new ListNode(1);
+        node.val = l1.val+l2.val;
+
+
+        return  result;
+
+
+
+
+
+
+    }
+}
