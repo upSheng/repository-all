a53f0749776af0c1cc81cdb34d2a2dee08c1bf2c
chs <fjndchs@163.com>
2019-12-07 02:46:20 +0800

    vue jwt

--- vue-admin/src/components/login.vue
+++ vue-admin/src/components/login.vue
@@ -1,13 +1,43 @@
 <template>
     <div>
-        login
+        <input type="text" v-model="username"></input>
+        <input type="text" v-model="password"></input>
+        <br>
+        <button @click="login">ok</button>
+        <button @click="check">check</button>
     </div>
 </template>
 
 <script>
     export default {
-        name: 'login'
-    }
+        name:'login',
+        data() {
+            return {
+                username: '',
+                password: ''
+            };
+        },
+        methods: {
+
+            login(){
+                console.log('vvv');
+                let param = new URLSearchParams()
+                param.append("username", "chs")
+                param.append("password", "123456")
+
+                this.axios.post('/api/login',param).then((response) => {
+                    console.log(response.data)
+
+                    localStorage.setItem("jwtToken",response.data.jwtToken);
+                })
+            },
+            check(){
+
+                var jwtToken = localStorage.getItem('jwtToken');
+                console.log(jwtToken);
+            }
+        }
+    };
 </script>
 
 
