dc24362ab10293d6bdd1e2c49b46449f7b5029ea
chs <fjndchs@163.com>
2019-12-25 21:03:16 +0800

    leetcode

--- leetcode/src/main/java/com/chs/Solution23.java
+++ leetcode/src/main/java/com/chs/Solution23.java
@@ -3,6 +3,19 @@ package com.chs;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
+/**
+ * 合并 k 个排序链表，返回合并后的排序链表。请分析和描述算法的复杂度。
+ *
+ * 示例:
+ *
+ * 输入:
+ * [
+ *   1->4->5,
+ *   1->3->4,
+ *   2->6
+ * ]
+ * 输出: 1->1->2->3->4->4->5->6
+ */
 public class Solution23 {
 
     public ListNode mergeKLists(ListNode[] lists){
