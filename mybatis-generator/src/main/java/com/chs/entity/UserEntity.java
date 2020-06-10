0fe4bc6f48044ab87b63130ab9b51f80464ece84
upsheng <chs18750307248cs>
2020-06-10 14:59:02 +0800

    mybatis-generator

---
+++ mybatis-generator/src/main/java/com/chs/entity/UserEntity.java
@@ -0,0 +1,65 @@
+package com.chs.entity;
+
+import java.util.Date;
+
+public class UserEntity {
+    private Integer id;
+
+    private Integer age;
+
+    private Date createTime;
+
+    private String createUser;
+
+    private String name;
+
+    private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(String createUser) {
+        this.createUser = createUser == null ? null : createUser.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+}
