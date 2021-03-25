a5523e2344d9fdf25f60198ea136c4519d111c98
upsheng <chs18750307248cs>
2021-03-25 18:44:56 +0800

    pay

---
+++ activity/src/main/java/com/chs/activity/config/Constans.java
@@ -0,0 +1,16 @@
+package com.chs.activity.config;
+
+/**
+ * @author : HongSheng.Chen
+ * @date : 2021/3/23 11:19
+ */
+public class Constans {
+
+    public static final String MCHID = "1547187151";
+    public static final String KEY = "**";
+    public static final String URL = "https://payjs.cn/api/native";
+    public static final String NOTIFY_URL = "http://1w7255a684.51mypc.cn/payHandle";
+
+    public static final Integer ORDER_STATUS_DEFAULT = 0;
+    public static final Integer ORDER_STATUS_SUCCESS = 1;
+}
